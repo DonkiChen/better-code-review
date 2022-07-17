f67de5e1769e88698102b3d198cb0813256d8ba8
GitHub Action <action@github.com>
2022-07-17 03:58:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/donkichen/bettercodereview/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.donkichen.bettercodereview.services
+
+import com.github.donkichen.bettercodereview.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/donkichen/bettercodereview/services/MyProjectService.kt
+++ src/main/kotlin/com/github/donkichen/bettercodereview/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.donkichen.bettercodereview.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.donkichen.bettercodereview.MyBundle
 
 class MyProjectService(project: Project) {
 
